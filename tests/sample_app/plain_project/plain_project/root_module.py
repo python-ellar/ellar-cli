6cd1396f178708c7e92255588716496e0e2abe4b
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2024-04-25 09:14:48 +0100

    upgrade to ellar 0.7.4 and added some test for dynamic command setup

--- tests/sample_app/plain_project/plain_project/root_module.py
+++ tests/sample_app/plain_project/plain_project/root_module.py
@@ -1,26 +1,33 @@
 from ellar.app import current_injector
 from ellar.common import (
     IExecutionContext,
+    IModuleSetup,
     JSONResponse,
     Module,
     Response,
     exception_handler,
 )
-from ellar.core import Config, ModuleBase
+from ellar.core import Config, DynamicModule, ModuleBase
 from ellar.samples.modules import HomeModule
 
 import ellar_cli.click as click
 
 
-@click.command()
-@click.with_app_context
-def plain_project():
-    """Project 2 Custom Command"""
-    assert isinstance(current_injector.get(Config), Config)
-    print("Plain Project Command works. Executed within application context")
+@Module()
+class DynamicCommandModule(ModuleBase, IModuleSetup):
+    @classmethod
+    def setup(cls) -> "DynamicModule":
+        @click.command()
+        @click.with_app_context
+        def plain_project():
+            """Project 2 Custom Command"""
+            assert isinstance(current_injector.get(Config), Config)
+            print("Plain Project Command works. Executed within application context")
+
+        return DynamicModule(cls, commands=[plain_project])
 
 
-@Module(modules=[HomeModule], commands=[plain_project])
+@Module(modules=[HomeModule, DynamicCommandModule.setup()])
 class ApplicationModule(ModuleBase):
     @exception_handler(404)
     def exception_404_handler(cls, ctx: IExecutionContext, exc: Exception) -> Response:
